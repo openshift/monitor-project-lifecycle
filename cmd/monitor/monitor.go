42e3c37b6c8f9a8943b25a3c559f2ee9036c20f9
Thomas Wiest <twiest@redhat.com>
2018-03-20 11:29:34 -0400

    Add the ability to clean up the workspace project

--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -7,7 +7,11 @@ import (
 	"net/http"
 	"time"
 
+	projectv1client "github.com/openshift/client-go/project/clientset/versioned/typed/project/v1"
 	"github.com/prometheus/client_golang/prometheus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
@@ -32,6 +36,42 @@ func main() {
 	go runAppCreateSim(appCreateLatency, 1*time.Second)
 
 	select {}
+}
+
+// Creates a rest config object that is used for other client calls.
+// TODO: we should probably not panic, instead expose this as an error in prometheus.
+func getRestConfig() *restclient.Config {
+	// Instantiate loader for kubeconfig file.
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		clientcmd.NewDefaultClientConfigLoadingRules(),
+		&clientcmd.ConfigOverrides{},
+	)
+
+	// Get a rest.Config from the kubeconfig file.  This will be passed into all
+	// the client objects we create.
+	restconfig, err := kubeconfig.ClientConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	return restconfig
+}
+
+// Cleans up all objects that this monitoring command creates
+// TODO: Add prometheus metrics for timing and errors.
+// TODO: Don't panic as that would kill the prometheus end point as well.
+func cleanupWorkspace(project string, restconfig *restclient.Config) {
+	// Create an OpenShift project/v1 client.
+	projectclient, err := projectv1client.NewForConfig(restconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// Delete the project that contains the kube resources we've created
+	err = projectclient.Projects().Delete(project, &metav1.DeleteOptions{})
+	if err != nil {
+		panic(err)
+	}
 }
 
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
