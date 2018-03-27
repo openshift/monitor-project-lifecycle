89d65202bedf34126132da317d5cdefb52f8cdad
gabemontero <gmontero@redhat.com>
2018-03-27 17:49:19 -0400

    monitor prototpe initial creation

--- cmd/monitor/monitor.go
+++ cmd/monitor/monitor.go
@@ -3,16 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
-	projectv1client "github.com/openshift/client-go/project/clientset/versioned/typed/project/v1"
+	//projectv1client "github.com/openshift/client-go/project/clientset/versioned/typed/project/v1"
 	"github.com/prometheus/client_golang/prometheus"
+
+	templatev1 "github.com/openshift/api/template/v1"
+	templatev1client "github.com/openshift/client-go/template/clientset/versioned/typed/template/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	//v1beta1 "k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	restclient "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
+
+type jenkinsSmokeTestCollector struct {
+	desc      *prometheus.Desc
+	stat      prometheus.Metric
+	namespace string
+}
+
+// Describe implements the prometheus.Collector interface.
+func (jc *jenkinsSmokeTestCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- jc.desc
+}
+
+// Collect implements the prometheus.Collector interface.
+func (jc *jenkinsSmokeTestCollector) Collect(ch chan<- prometheus.Metric) {
+	ch <- jc.stat
+}
+
+func (jc *jenkinsSmokeTestCollector) successMetric() {
+	lv := []string{jc.namespace, "success", ""}
+	jc.stat = prometheus.MustNewConstMetric(jc.desc, prometheus.GaugeValue, float64(time.Now().Unix()), lv...)
+}
+
+func (jc *jenkinsSmokeTestCollector) failureMetric(errStr string) {
+	lv := []string{jc.namespace, "failure", errStr}
+	jc.stat = prometheus.MustNewConstMetric(jc.desc, prometheus.GaugeValue, float64(time.Now().Unix()), lv...)
+}
 
 func main() {
 	addr := flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
@@ -21,46 +54,155 @@ func main() {
 	http.HandleFunc("/healthz", handleHealthz)
 	http.Handle("/metrics", prometheus.Handler())
 
-	appCreateLatency := prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name:    "app_create_latency_seconds",
-			Help:    "The latency of various app creation steps.",
-			Buckets: []float64{1, 10, 60, 3 * 60, 5 * 60},
-		},
-		[]string{"step"},
+	collector := jenkinsSmokeTestCollector{}
+	collector.desc = prometheus.NewDesc(
+		"jenkins_smoke_test_seconds",
+		"Shows the time in unix epoch the jenkins smoke test was run by namespace, status, and reason",
+		[]string{"namespace", "status", "reason"},
+		nil,
 	)
-	prometheus.MustRegister(appCreateLatency)
+
+	prometheus.MustRegister(&collector)
 
 	go http.ListenAndServe(*addr, nil)
 
-	go runAppCreateSim(appCreateLatency, 1*time.Second)
+	go runAppCreateSim(collector, 10*time.Minute)
 
 	select {}
 }
 
-// Creates a rest config object that is used for other client calls.
-// TODO: we should probably not panic, instead expose this as an error in prometheus.
-func getRestConfig() *restclient.Config {
-	// Instantiate loader for kubeconfig file.
-	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		clientcmd.NewDefaultClientConfigLoadingRules(),
-		&clientcmd.ConfigOverrides{},
-	)
-
-	// Get a rest.Config from the kubeconfig file.  This will be passed into all
-	// the client objects we create.
-	restconfig, err := kubeconfig.ClientConfig()
+func instantiateJenkins(templateclient *templatev1client.TemplateV1Client, coreclient *corev1client.CoreV1Client, namespace string) error {
+	// Get the "jenkins-ephemeral" Template from the "openshift" Namespace.
+	template, err := templateclient.Templates("openshift").Get(
+		"jenkins-ephemeral", metav1.GetOptions{})
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	return restconfig
+	// INSTANTIATE THE TEMPLATE.
+
+	// To set Template parameters, create a Secret holding overridden parameters
+	// and their values.
+	secret, err := coreclient.Secrets(namespace).Create(&corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "parameters",
+		},
+		StringData: map[string]string{
+			"MEMORY_LIMIT": "1024Mi",
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	// Create a TemplateInstance object, linking the Template and a reference to
+	// the Secret object created above.
+	ti, err := templateclient.TemplateInstances(namespace).Create(
+		&templatev1.TemplateInstance{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "templateinstance",
+			},
+			Spec: templatev1.TemplateInstanceSpec{
+				Template: *template,
+				Secret: &corev1.LocalObjectReference{
+					Name: secret.Name,
+				},
+			},
+		})
+	if err != nil {
+		return err
+	}
+
+	// Watch the TemplateInstance object until it indicates the Ready or
+	// InstantiateFailure status condition.
+	watcher, err := templateclient.TemplateInstances(namespace).Watch(
+		metav1.SingleObject(ti.ObjectMeta),
+	)
+	if err != nil {
+		return err
+	}
+
+	for event := range watcher.ResultChan() {
+		switch event.Type {
+		case watch.Modified:
+			ti = event.Object.(*templatev1.TemplateInstance)
+
+			for _, cond := range ti.Status.Conditions {
+				// If the TemplateInstance contains a status condition
+				// Ready == True, stop watching.
+				if cond.Type == templatev1.TemplateInstanceReady &&
+					cond.Status == corev1.ConditionTrue {
+					watcher.Stop()
+				}
+
+				// If the TemplateInstance contains a status condition
+				// InstantiateFailure == True, indicate failure.
+				if cond.Type ==
+					templatev1.TemplateInstanceInstantiateFailure &&
+					cond.Status == corev1.ConditionTrue {
+					return fmt.Errorf("templateinstance instantiation failed")
+				}
+			}
+
+		default:
+			return fmt.Errorf("unexpected event type %s", string(event.Type))
+		}
+	}
+
+	return nil
+}
+
+func getTemplateClient(restconfig *restclient.Config) (*templatev1client.TemplateV1Client, error) {
+	return templatev1client.NewForConfig(restconfig)
+}
+
+func getCoreClient(restconfig *restclient.Config) (*corev1client.CoreV1Client, error) {
+	return corev1client.NewForConfig(restconfig)
+}
+
+func getRestConfig() (*restclient.Config, error) {
+	// Build a rest.Config from configuration injected into the Pod by
+	// Kubernetes.  Clients will use the Pod's ServiceAccount principal.
+	return restclient.InClusterConfig()
+}
+
+func getNamespace() string {
+	return os.Getenv("NAMESPACE")
+}
+
+func listPods(coreclient *corev1client.CoreV1Client, namespace string) error {
+	pods, err := coreclient.Pods(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Pods in namespace %s:\n", namespace)
+	for _, pod := range pods.Items {
+		fmt.Printf("  %s\n", pod.Name)
+	}
+
+	return nil
+}
+
+func scaleJenkins(coreclient *corev1client.CoreV1Client, namespace string, up bool) error {
+	scale, err := coreclient.ReplicationControllers(namespace).GetScale("jenkins",
+		metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	if up {
+		scale.Spec.Replicas = 0
+	} else {
+		scale.Spec.Replicas = 1
+	}
+
+	_, err = coreclient.ReplicationControllers(namespace).UpdateScale("jenkins", scale)
+	return err
 }
 
 // Cleans up all objects that this monitoring command creates
-// TODO: Add prometheus metrics for timing and errors.
-// TODO: Don't panic as that would kill the prometheus end point as well.
-func cleanupWorkspace(project string, restconfig *restclient.Config) {
+/*func cleanupWorkspace(project string, restconfig *restclient.Config) error {
 	// Create an OpenShift project/v1 client.
 	projectclient, err := projectv1client.NewForConfig(restconfig)
 	if err != nil {
@@ -72,28 +214,103 @@ func cleanupWorkspace(project string, restconfig *restclient.Config) {
 	if err != nil {
 		panic(err)
 	}
-}
+}*/
 
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ok")
 }
 
-func runAppCreateSim(metric *prometheus.HistogramVec, interval time.Duration) {
-	steps := map[string]struct {
-		min time.Duration
-		max time.Duration
-	}{
-		"new-app": {min: 1 * time.Second, max: 5 * time.Second},
-		"build":   {min: 1 * time.Minute, max: 5 * time.Minute},
-		"deploy":  {min: 1 * time.Minute, max: 5 * time.Minute},
-		"expose":  {min: 10 * time.Second, max: 1 * time.Minute},
-	}
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for {
-		for step, r := range steps {
-			latency := rng.Int63n(int64(r.max)-int64(r.min)) + int64(r.min)
-			metric.With(prometheus.Labels{"step": step}).Observe(float64(latency / int64(time.Second)))
+func runAppCreateSim(collector jenkinsSmokeTestCollector, interval time.Duration) {
+
+	var templateclient *templatev1client.TemplateV1Client
+	var coreclient *corev1client.CoreV1Client
+	var namespace string
+	restconfig, err := getRestConfig()
+	if err == nil {
+		collector.namespace = getNamespace()
+		fmt.Printf("namesapce is %s\n", collector.namespace)
+		templateclient, err = getTemplateClient(restconfig)
+		if err == nil {
+			coreclient, err = getCoreClient(restconfig)
+			err = instantiateJenkins(templateclient, coreclient, namespace)
+			if err == nil {
+				listPods(coreclient, namespace)
+			}
 		}
+	}
+	instantiated := true
+	if err != nil {
+		instantiated = false
+		errStr := fmt.Sprintf("setup error %#v", err)
+		fmt.Printf("%s\n", errStr)
+		collector.failureMetric(errStr)
+	} else {
+		collector.successMetric()
+	}
+
+	for {
 		time.Sleep(interval)
+		if !instantiated {
+			if restconfig == nil {
+				restconfig, err = getRestConfig()
+				if err != nil {
+					errStr := fmt.Sprintf("setup error %#v", err)
+					fmt.Printf("%s\n", errStr)
+					collector.failureMetric(errStr)
+					continue
+				}
+				collector.namespace = getNamespace()
+				fmt.Printf("namesapce is %s\n", collector.namespace)
+			}
+
+			if templateclient == nil {
+				templateclient, err = getTemplateClient(restconfig)
+				if err != nil {
+					errStr := fmt.Sprintf("setup error %#v", err)
+					fmt.Printf("%s\n", errStr)
+					collector.failureMetric(errStr)
+					continue
+				}
+			}
+
+			if coreclient == nil {
+				coreclient, err = getCoreClient(restconfig)
+				if err != nil {
+					errStr := fmt.Sprintf("setup error %#v", err)
+					fmt.Printf("%s\n", errStr)
+					collector.failureMetric(errStr)
+					continue
+				}
+			}
+
+			if !instantiated {
+				err = instantiateJenkins(templateclient, coreclient, namespace)
+				if err != nil {
+					errStr := fmt.Sprintf("setup error %#v", err)
+					fmt.Printf("%s\n", errStr)
+					collector.failureMetric(errStr)
+					continue
+				}
+				listPods(coreclient, namespace)
+			}
+		}
+
+		err = scaleJenkins(coreclient, namespace, false)
+		if err != nil {
+			errStr := fmt.Sprintf("setup error %#v", err)
+			fmt.Printf("%s\n", errStr)
+			collector.failureMetric(errStr)
+			continue
+		}
+
+		err = scaleJenkins(coreclient, namespace, true)
+		if err != nil {
+			errStr := fmt.Sprintf("setup error %#v", err)
+			fmt.Printf("%s\n", errStr)
+			collector.failureMetric(errStr)
+			continue
+		}
+
+		collector.successMetric()
 	}
 }
